92088d88cad22b67330b093f42c39a329886a86f
dinhorodrigues <glebson_lg@hotmail.com>
2020-06-18 00:09:48 -0300

    Componentes do Menu

---
+++ frontEnd/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props=>(
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2020
+            <a href="www.google.com.br" target='_black'>  DinhoRodrigues</a>
+        </strong>
+    </footer>
+)
--- frontEnd/src/main/app.jsx
+++ frontEnd/src/main/app.jsx
@@ -1,11 +1,21 @@
-import '../common/template/dependencies.js'
-import Header from  '../common/template/header.jsx'
+import '../common/template/dependencies'
+
 import React from 'react'
+import Header from  '../common/template/header'
+import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 
 export default props => (
     <div className ='wrapper'>
-        <Header />
+        <Header/>
+        <SideBar/>
+        <div className='content-wrapper'>
+            <h1>Conteudo</h1>
+
+        </div>
+        <Footer/>
+       
 
     </div>
 )
