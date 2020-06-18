68248cae42d181294742dd4d161032d01161be71
dinhorodrigues <glebson_lg@hotmail.com>
2020-06-17 21:30:56 -0300

    criação do menu

---
+++ frontEnd/src/main/app.jsx
@@ -0,0 +1,11 @@
+import '../common/template/dependencies.js'
+import Header from  '../common/template/header.jsx'
+import React from 'react'
+
+
+export default props => (
+    <div className ='wrapper'>
+        <Header />
+
+    </div>
+)
